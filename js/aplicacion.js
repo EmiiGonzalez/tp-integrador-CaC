922cbbe83080488601f305571af01f145c2a4434
Emiliano Gonzalez <emigonzalez3333@gmail.com>
2023-05-13 20:18:05 -0300

    Agregado de nuevas secciones

--- js/aplicacion.js
+++ js/aplicacion.js
@@ -1,9 +1,6 @@
 import { validar } from "./validacion.js";
 const inputs = document.querySelectorAll("input");
 
-inputs.forEach(input => {
-    input.addEventListener("blur", (evento) => {
-        validar(evento.target)
-    })
-})
-
+inputs.forEach((input) => {
+  input.addEventListener("blur", validar);
+});
--- js/validacion.js
+++ js/validacion.js
@@ -1,26 +1,35 @@
 export const validar = (input) =>{
-    const tipoDeInput = input.dataset.tipo;
-    const span = input.nextSibling;
-    if(input.validity.valid){
-        console.log(span);
-        span.classList.add("input-message-error");
-        span.innerHTML = "";
+    const elemento = input.target;
+    const valorElemento = elemento.value.trim().length;
+    const largoBoolean = valorElemento >= 3 && valorElemento <= 25
+    const tipoDeInput = elemento.dataset.tipo;
+    const span = elemento.nextElementSibling;
+    console.log(elemento.validity.valid && largoBoolean);
+    if(elemento.validity.valid && largoBoolean){
+        elemento.classList.remove("campo-error")
+        span.classList.remove("error")
+        span.innerText  = "";
+        console.log("entro");
     }else {
-        console.log(span);
-        span.classList.remove("input-message-error");
-        span.innerHTML = mostrarMensajeDeError(tipoDeInput, input);
+        elemento.classList.add("campo-error")
+        span.classList.add("error")
+        span.innerText  = mostrarMensajeDeError(tipoDeInput, elemento);
+        elemento.setCustomValidity(mensajesDeError[tipoDeInput].errorPersonalizado)
+        console.log("no entro");
     }
 }
 
-const tipoDeErrores = ["valueMissing", "typeMismatch", "patternMismatch", "customError"];
+const tipoDeErrores = ["valueMissing", "typeMismatch"];
 
 const mensajesDeError = {
     nombre: {
-        valueMissing: "El campo nombre no puede estar vacio"
+        valueMissing: "El campo nombre no puede estar vacio",
+        errorPersonalizado: "El nombre debe ser mayor a 3 letras y menor a 25"
     },
     email: {
         valueMissing: "El campo correo no puede estar vacio",
-        typeMismatch: "El correo no es valido"
+        errorPersonalizado: "El correo ingresado no es valido",
+        typeMismatch: "El formato del correo no es valido"
     }
 };
 
