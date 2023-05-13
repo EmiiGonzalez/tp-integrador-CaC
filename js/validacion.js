237532148d01a7ccca662c6ac4d84f47c66288a3
Emiliano Gonzalez <emigonzalez3333@gmail.com>
2023-05-13 20:54:29 -0300

    v1

--- js/validacion.js
+++ js/validacion.js
@@ -9,13 +9,10 @@ export const validar = (input) =>{
         elemento.classList.remove("campo-error")
         span.classList.remove("error")
         span.innerText  = "";
-        console.log("entro");
     }else {
         elemento.classList.add("campo-error")
         span.classList.add("error")
         span.innerText  = mostrarMensajeDeError(tipoDeInput, elemento);
-        elemento.setCustomValidity(mensajesDeError[tipoDeInput].errorPersonalizado)
-        console.log("no entro");
     }
 }
 
@@ -24,11 +21,9 @@ const tipoDeErrores = ["valueMissing", "typeMismatch"];
 const mensajesDeError = {
     nombre: {
         valueMissing: "El campo nombre no puede estar vacio",
-        errorPersonalizado: "El nombre debe ser mayor a 3 letras y menor a 25"
     },
     email: {
         valueMissing: "El campo correo no puede estar vacio",
-        errorPersonalizado: "El correo ingresado no es valido",
         typeMismatch: "El formato del correo no es valido"
     }
 };
